7565989afbf942f389c04c25887a96f12a45be04
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-05-23 10:06:00 +0530

    sealed demo

--- C#Tests/Program.cs
+++ C#Tests/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using C_Tests.SealedTest;
+using static System.Console;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DemoSealed();
+
+    }
+
+    private static void DemoSealed()
+    {
+        IEnumerable<Parent> objs = new List<Parent> { new(), new ChildGenOne(), new ChildGenTwo() };
+        foreach (var item in objs)
+        {
+            WriteLine(item);
+        }
+    }
+}
---
+++ C#Tests/SealedTest/Parent.cs
@@ -0,0 +1,33 @@
+namespace C_Tests.SealedTest
+{
+    public class Parent
+    {
+        public virtual string  M(){
+            return "Parent";
+        }
+
+        protected string GetClassNameStr()
+        {
+            return GetType().Name;
+        }
+
+        public override string ToString()
+        {
+            return $"{GetClassNameStr()} - {M()}";
+        }
+    }
+
+    public class ChildGenOne : Parent
+    {
+         sealed override public string M(){
+            return "Child GenOne - made it sealed, so end of overriding";
+        }
+    }
+
+    public class ChildGenTwo : ChildGenOne
+    {
+        // override public string M(){
+        //     Console.WriteLine("Child GenTwo");
+        // }
+    }
+}
