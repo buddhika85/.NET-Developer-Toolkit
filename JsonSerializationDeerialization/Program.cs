10e57c7c56d3e9b05e94bf6d5fe702f923283c98
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-06-01 03:07:23 +1000

    json serialization

--- JsonSerializationDeerialization/Program.cs
+++ JsonSerializationDeerialization/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+using JsonSerializationDeerialization.Models;
+using System.Text.Json;
+
+var person = new Person {
+    Id = 1,
+    FirstName = "James",
+    LastName = "Gunn",
+    Age = 90,
+    IsAlive = true
+};
+var opt = new JsonSerializerOptions 
+{
+    WriteIndented = true,
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+};
+
+var jsonString = JsonSerializer.Serialize<Person>(person, opt);
+// display
+WriteLine(jsonString);
+
+// write to a file
+var path = "C:\\BUDDHIKA\\Development\\Git\\.NET-Developer-Toolkit\\JsonSerializationDeerialization\\person.json";
+File.WriteAllText(path, jsonString);
+WriteLine(File.ReadAllText(path));
