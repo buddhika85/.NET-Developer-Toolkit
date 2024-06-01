d987b9115836a294ea7227455bf83f86c732b808
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-06-01 15:19:49 +1000

    get methods

--- RepositoryPattern/CommandAPINoRepository/Program.cs
+++ RepositoryPattern/CommandAPINoRepository/Program.cs
@@ -1,4 +1,6 @@
 using CommandAPINoRepository.Data;
+using CommandAPINoRepository.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +18,35 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// get multiple
+app.MapGet("api/v1/commands", async (AppDbContext context) => {
+    var commands = await context.Commands.ToListAsync();
+    return Results.Ok(commands);
+});
+
+// get single
+app.MapGet("api/v1/commands/{id}", async (AppDbContext context, string id) => {
+    var command = await context.Commands.FirstOrDefaultAsync(x => x.CommandId == id);
+    if (command == null)
+        return Results.NotFound($"No such command with {id}");
+    return Results.Ok(command);
+});
+
+// // create
+// app.MapGet("api/v1/commands", async (AppDbContext context) => {
+    
+// });
+
+// // update
+// app.MapGet("api/v1/commands", async (AppDbContext context) => {
+    
+// });
+
+// // delete
+// app.MapGet("api/v1/commands", async (AppDbContext context) => {
+    
+// });
 
 app.UseHttpsRedirection();
 app.Run();
