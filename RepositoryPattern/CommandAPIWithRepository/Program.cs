98f0f99abc545d8eafd6b1a8240107f1665b216d
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-06-01 17:00:35 +1000

    IRepo concrete implementation for SQL server

--- RepositoryPattern/CommandAPIWithRepository/Program.cs
+++ RepositoryPattern/CommandAPIWithRepository/Program.cs
@@ -1,6 +1,6 @@
 using CommandAPINoRepository.Data;
 using CommandAPINoRepository.Models;
-using Microsoft.AspNetCore.Http.HttpResults;
+using CommandAPIWithRepository.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("SQLDbConnection")));  // 
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("SQLDbConnection")));  
+builder.Services.AddScoped<ICommandRepo, SqlCommandRepo>();
+
 
 var app = builder.Build();
 
@@ -20,23 +22,22 @@ if (app.Environment.IsDevelopment())
 }
 
 // get multiple
-app.MapGet("api/v1/commands", async (AppDbContext context) => {
-    var commands = await context.Commands.ToListAsync();
-    return Results.Ok(commands);
+app.MapGet("api/v1/commands", async (ICommandRepo repo) => {   
+    return Results.Ok(await repo.GetAllAsync());
 });
 
 // get single
-app.MapGet("api/v1/commands/{id}", async (AppDbContext context, int id) => {
-    var command = await context.Commands.FirstOrDefaultAsync(x => x.Id == id);
+app.MapGet("api/v1/commands/{id}", async (ICommandRepo repo, int id) => {
+    var command = await repo.GetByIdAsync(id);
     if (command == null)
         return Results.NotFound($"No such command with {id}");
     return Results.Ok(command);
 });
 
 // create
-app.MapPost("api/v1/commands", async (AppDbContext context, Command command) => {
-    await context.Commands.AddAsync(command);
-    await context.SaveChangesAsync();
+app.MapPost("api/v1/commands", async (ICommandRepo repo, Command command) => {
+    await repo.CreateAsync(command);
+    await repo.SaveChanges();
     // rest conventions - rout to access new command and new command object
     return Results.Created($"api/v1/commands/{command.Id}", command);
 });
@@ -58,13 +59,13 @@ app.MapPut("api/v1/commands/{id}", async (AppDbContext context, int id, Command 
 });
 
 // delete
-app.MapDelete("api/v1/commands", async (AppDbContext context, int id) => {
-    var commandToDelete = await context.Commands.FirstOrDefaultAsync(x => x.Id == id);
+app.MapDelete("api/v1/commands", async (ICommandRepo repo, int id) => {
+    var commandToDelete = await repo.GetByIdAsync(id);
     if (commandToDelete == null)
         return Results.NotFound($"No such command with {id}");
 
-    context.Commands.Remove(commandToDelete);
-    await context.SaveChangesAsync();
+    repo.Delete(commandToDelete);
+    await repo.SaveChanges();
 
     // return resouce which was deleted
     return Results.Ok(commandToDelete);
