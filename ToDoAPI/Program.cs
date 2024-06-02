a5b22753253a44b0d63fddf6bea6fe6cd1e2e6ab
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-06-02 18:16:20 +1000

    added API end points

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ToDoAPI.Data;
+using ToDoAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,27 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("ToDoDB"));
 
 var app = builder.Build();
+
+// get all
+app.MapGet("api/todo", async(AppDbContext context) => {
+    return Results.Ok(await context.ToDos.ToListAsync());
+});
+
+// get by Id
+app.MapGet("api/todo/{id}", async(AppDbContext context, int id) => {
+    ToDo? todo = await  context.ToDos.FirstOrDefaultAsync(x => x.Id == id);
+    if (todo == null) 
+        return Results.NotFound($"No ToDo with ID {id}");
+    return Results.Ok(todo);
+});
+
+// create
+app.MapPost("api/todo", async(AppDbContext context, ToDo todo) => {
+    await context.ToDos.AddAsync(todo);
+    await context.SaveChangesAsync();
+    // passing read DTO from created response
+    return Results.Created($"api/todo/{todo.Id}", todo);
+});
 
 
 if (app.Environment.IsDevelopment())
