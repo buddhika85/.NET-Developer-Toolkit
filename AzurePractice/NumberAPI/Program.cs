3f59bb4543f4487dd8754fe1da9ef0cd162224e9
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-06-14 04:35:36 +1000

    API changes

--- AzurePractice/NumberAPI/Program.cs
+++ AzurePractice/NumberAPI/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using NumberAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("Numbers"));
 
 var app = builder.Build();
 
