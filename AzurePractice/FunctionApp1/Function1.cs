9e9540a082c15c211cd2547251a1039500bcaa6b
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-05-23 11:13:37 +1000

    example azure function

---
+++ AzurePractice/FunctionApp1/Function1.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionApp1
+{
+    public class Function1
+    {
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("Function1")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            var name = req.Query["name"];
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                name = "Guest";
+            }
+
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            return new OkObjectResult($"Welcome to Azure Functions! - {name}");
+        }
+    }
+}
